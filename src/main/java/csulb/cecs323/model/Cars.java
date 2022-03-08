804d5baa291216250697e54d7aca94764d9e5775
kennydus <kennydus@gmail.com>
2022-03-07 16:12:23 -0800

    Some comments added

--- src/main/java/csulb/cecs323/model/Cars.java
+++ src/main/java/csulb/cecs323/model/Cars.java
@@ -1,11 +1,20 @@
 package csulb.cecs323.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 /**
  * Individual, physical automobiles that someone can drive on land to transport one or more passengers
  * and a limited amount of cargo around.  Cars have four wheels and usually travel on paved roads.
  */
 public class Cars {
-    /** The unique ID of the vehicle.  Limited to 17 characters. */
+
+    /** The owner of the car. Limited to 40 characters. */
+    private Owners owner;
+
+    /** The unique ID of the vehicle. Limited to 17 characters. */
     private String VIN;
 
     /** The name of the corporation which manufactured the vehicle.  Limited to 40 characters. */
@@ -16,6 +25,13 @@ public class Cars {
 
     /** The year that the vehicle was manufactured.  For now, do not worry about validating this #. */
     private int year;
+
+    /** The body style of the car. */
+    private auto_body_styles auto_body_style;
+
+    public Cars() {
+    }
+
 
     @Override
     public String toString () {
